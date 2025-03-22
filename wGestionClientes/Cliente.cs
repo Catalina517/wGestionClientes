443fea8a71f105ed99136f6f4d7092832629a023
Catalina <catalina.correa346@pascualbravo.edu.co>
2025-03-22 17:37:39 -0500

    Se agregó la clase Cliente

---
+++ wGestionClientes/Cliente.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wGestionClientes
+{
+    public abstract class Cliente
+    {
+        public string Nombre { get; set; }
+        public string Identificacion { get; set; }
+
+        public decimal Saldo { get; set; }
+
+        public Cliente(string nombre, string identificacion, decimal saldo)
+        {
+            if (string.IsNullOrWhiteSpace(nombre) || string.IsNullOrWhiteSpace(identificacion))
+            {
+                throw new ArgumentNullException("El nombre no puede estar vacío");
+            }
+            if (saldo <= 0)
+            {
+                throw new ArgumentException("El saldo debe ser mayor que 0");
+            }
+
+            Nombre = nombre;
+            Identificacion = identificacion;
+            Saldo = saldo;
+        }
+
+        public abstract string CalcularBeneficio();
+    }
+}
