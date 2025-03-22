8bc41eddcf9614d0a8a3883327ce90301071e5ad
Catalina <catalina.correa346@pascualbravo.edu.co>
2025-03-22 18:09:17 -0500

    Se crea la clase ClienteIndividual

---
+++ wGestionClientes/ClienteIndividual.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wGestionClientes
+{
+    public class ClienteIndividual : Cliente
+    {
+        public int CantidadCuentasActivas {  get; set; }
+
+        public ClienteIndividual(string nombre, string identificacion, decimal saldo, int cantidadCuentas) : base(nombre, identificacion, saldo)
+        {
+            if (cantidadCuentas > 3)
+            {
+                MessageBox.Show("Un cliente individual no puede tener más de 3 cuentas activas", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                CantidadCuentasActivas = cantidadCuentas;
+            }
+        }
+
+        public override string CalcularBeneficio()
+        {
+            return "Cliente individual sin beneficios especiales.";
+        }
+    }
+}
