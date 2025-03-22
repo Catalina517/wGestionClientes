ff2514162311d28f9ef331b849fb839dea76e17f
Catalina <catalina.correa346@pascualbravo.edu.co>
2025-03-22 17:51:24 -0500

    Se crea la clase derivada ClienteCorportativo

--- wGestionClientes/ClienteCorporativo.cs
+++ wGestionClientes/ClienteCorporativo.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace wGestionClientes
 {
-    public class ClienteCorporativo
+    public class ClienteCorporativo : Cliente
     {
+        public bool AccesoLineaCredito {  get; set; }
+
+        public ClienteCorporativo(string nombre, string identificacion, decimal saldo) : base(nombre, identificacion, saldo)
+        {
+            AccesoLineaCredito = saldo > 50000000;
+
+            if (!AccesoLineaCredito)
+            {
+                MessageBox.Show("El saldo no alcanza para acceder al crédito", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        public override string CalcularBeneficio()
+        {
+            if (AccesoLineaCredito)
+            {
+                return "Cliente con acceso a línea de crédito.";
+            }
+            else
+            {
+                return "Cliente sin acceso a línea de crédito.";
+            }
+        }
     }
 }
